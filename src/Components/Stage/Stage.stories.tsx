c5640e213c8210611864f508eeb2db3f7e9cbaea
Julia Reinhold <julia.reinhold@bitgrip.de>
2022-05-09 07:36:14 +0200

    Stage component

---
+++ src/Components/Stage/Stage.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Stage } from "./Stage";
+import { StageMock } from "./Stage.mock";
+
+export default {
+  title: "Stage",
+  component: Stage,
+} as ComponentMeta<typeof Stage>;
+
+export const Primary: ComponentStory<typeof Stage> = () => (
+  <Stage stage={StageMock} />
+);
+Primary.parameters = {
+  chromatic: { viewports: [450, 760, 1000, 1400] },
+};
+Primary.story = { name: "Stage" };
