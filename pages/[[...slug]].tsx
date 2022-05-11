738f72e0a56322dd55208cd9e30f22a8f316a56c
Julia Reinhold <julia.reinhold@bitgrip.de>
2022-05-11 08:40:25 +0200

    update page structure

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -4,12 +4,10 @@ import Head from "next/head";
 import { join } from "lodash";
 import { Header } from "../src/Components/Header/Header";
 import { Navigation } from "../src/types/Navigation/Navigation";
+import { Footer } from "../src/Components/Footer/Footer";
+import { Container } from "../src/Components/Container/Container";
+import { Row } from "../src/Components/Row/Row";
 import { Page } from "../src/types/layout/Page";
-import RelatedSection from "../src/Components/RelatedSection/RelatedSection";
-import {
-  getNavigation,
-  NavigationResponse,
-} from "../src/StoryblokClient/Queries/GetNavigation/getNavigation";
 import { JobSearch } from "../src/Components/JobSearch/JobSearch";
 import {
   filterList,
@@ -24,7 +22,6 @@ import { IContentElement } from "../src/types/layout/ContentElement";
 
 export interface SSRPageLayoutProps {
   slug: string;
-  navigation: Navigation | null;
   pageContent: Page | null;
 }
 
@@ -34,12 +31,10 @@ export const getStaticProps: GetStaticProps<SSRPageLayoutProps> = async ({
   const slug = join(params!.slug, "/");
   console.debug("getStaticProps for " + slug);
 
-  let navigation: NavigationResponse | undefined = undefined;
   let pageContent: PageContentResponse | undefined = undefined;
 
   try {
-    [navigation, pageContent] = await Promise.all([
-      getNavigation(),
+    [pageContent] = await Promise.all([
       getPageContent({ slug }),
     ]);
   } catch (error) {
@@ -49,7 +44,6 @@ export const getStaticProps: GetStaticProps<SSRPageLayoutProps> = async ({
   return {
     props: {
       slug: slug,
-      navigation: navigation?.data || null,
       pageContent: pageContent?.data || null,
     },
     revalidate: 3600,
@@ -61,8 +55,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export default function PageLayout(props: SSRPageLayoutProps) {
-  const { slug, navigation, pageContent } = props;
+  const { slug, pageContent } = props;
 
+  /*
   if (!navigation) {
     return (
       <div className="absolute top-0 grid grid-cols-1 h-full min-h-full w-full min-w-full place-content-center content-center text-center bg-gray-50">
@@ -72,6 +67,7 @@ export default function PageLayout(props: SSRPageLayoutProps) {
       </div>
     );
   }
+  */
 
   return (
     <div className="bg-white">
@@ -80,33 +76,19 @@ export default function PageLayout(props: SSRPageLayoutProps) {
         <meta name="description" content={`Hallo`} />
         <link rel="canonical" href={`/fghjk/`} />
       </Head>
-      <Header />
-      <div className="min-h-[calc(100vh_-_15rem)] py-8">
-        <main
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-16"
-          key="pageSection"
-        ></main>
-        {/* <pre>{JSON.stringify(navigation, null, 2)}</pre> */}
-        {pageContent?.title && <h1>{pageContent.title}</h1>}
-        {/* <pre>{JSON.stringify(pageContent, null, 2)}</pre> */}
-
-        {pageContent?.content &&
-          pageContent?.content.map((item) => {
-            return ContentElementComponentMapper(item);
-          })}
-
-        {slug === "jobs" && (
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-16">
-            <JobSearch list={jobList} filterList={filterList} />
-          </div>
-        )}
-        <Suspense fallback={<div>Loading...</div>}>
-          <aside className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-16">
-            <RelatedSection path={slug} />
-          </aside>
-        </Suspense>
-      </div>
-      {/* <Footer /> */}
+      <body className="body--general">
+        <Header />
+        <div className="content-wrapper">
+          <Container>
+            <Row>
+              {slug === "jobs" && (
+                <JobSearch list={jobList} filterList={filterList} />
+              )}
+            </Row>
+          </Container>
+        </div>
+        <Footer />
+      </body>
     </div>
   );
 }
