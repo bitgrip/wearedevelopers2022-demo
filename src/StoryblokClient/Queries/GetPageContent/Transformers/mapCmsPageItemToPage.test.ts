5e18693142aa277f7b65cf84630faa31472a2b8f
Daniel Ebert <daniel.ebert@bitgrip.de>
2022-05-13 14:21:54 +0200

    fix mapping test

--- src/StoryblokClient/Queries/GetPageContent/Transformers/mapCmsPageItemToPage.test.ts
+++ src/StoryblokClient/Queries/GetPageContent/Transformers/mapCmsPageItemToPage.test.ts
@@ -9,6 +9,6 @@ describe("should transform a cms page item into an app page item type", () => {
     const page = mapCmsPageItemToPage(contentPage);
     expect(page).toHaveProperty("id", "b9ce7e90-ed21-4547-98f3-70c01c1dfbcb");
     expect(page).toHaveProperty("slug", "jobs/");
-    expect(page).toHaveProperty("title", "JobOffers");
+    expect(page).toHaveProperty("title", "Job Offers");
   });
 });
