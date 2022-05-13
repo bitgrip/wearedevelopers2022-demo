112160cd58b968257eb206c24ebc23f76a384620
Karina Nowicka <karina.nowicka@bitgrip.de>
2022-05-13 16:00:23 +0200

    Add test for filtering. Add test group

--- src/Components/Header/Header.test.tsx
+++ src/Components/Header/Header.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from '../../test/test-utils';
 import { Header } from './Header';
 
+/**
+ * @group func
+ */
+
 test('should render logo', () => {
   render(<Header />);
   expect(screen.getByText('The Bitgrip Company Logo')).toBeInTheDocument();
--- src/Components/JobSearch/JobSearch.test.tsx
+++ src/Components/JobSearch/JobSearch.test.tsx
@@ -1,13 +1,17 @@
-import { render, screen } from '../../test/test-utils';
+import { render, screen  } from '../../test/test-utils';
+import userEvent from '@testing-library/user-event';
 import { JobSearch } from './JobSearch';
 import { multi } from './JobSearch.mock';
 
+/**
+ * @group func
+ */
+
+beforeEach(() => {
+  render(<JobSearch { ...multi } />);
+});
 
 describe('buttons', () => {
-  beforeEach(() => {
-    render(<JobSearch { ...multi } />);
-  });
-
   test('should render 8 buttons', () => {
     const buttons = screen.getAllByRole('button');
     expect(buttons).toHaveLength(8);
@@ -16,5 +20,20 @@ describe('buttons', () => {
   test('the first button should have label "all"', () => {
     const buttons = screen.getAllByRole('button');
     expect(buttons[0]).toHaveTextContent('all');
+  });
+});
+
+describe('filtering', () => {
+  test('should filter frontend jobs', async () => {
+    const frontendButton = screen.getByRole('button', { name: 'Frontend' });
+    const allButton = screen.getByRole('button', { name: 'all' });
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(7);
+
+    await userEvent.click(frontendButton);
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
+
+    await userEvent.click(allButton);
+    expect(screen.getAllByRole('listitem')).toHaveLength(7);
   });
 });
