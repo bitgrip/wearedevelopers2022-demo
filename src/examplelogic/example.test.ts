f1d8b152f39973269892e62390ffcb71ef2ab81d
Daniel Ebert <daniel.ebert@bitgrip.de>
2022-05-27 07:35:34 +0200

    Revert "push a failing test"
    This reverts commit 77a3322100640fb00ae447fc04077eb21031cb70.

--- src/examplelogic/example.test.ts
+++ src/examplelogic/example.test.ts
@@ -7,8 +7,4 @@ describe("example tests", () => {
     test("foo should return 'bar'", () => {
         expect(foo()).toBe("bar");
     });
-
-    test("ouch - this one will fail", () => {
-        expect(foo()).toBe("brrr");
-    });
 });
