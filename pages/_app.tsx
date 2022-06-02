03dd2fd42cefebd1877fb3863e96b0b892a53f60
Sven Friedemann <sven.friedemann@bitgrip.de>
2022-06-02 16:13:58 +0200

    Remove swagger doc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 
-function WebApp({ Component, pageProps }: AppProps) {
+function WebApp({ Component, pageProps }: any) {
   return <Component {...pageProps} />;
 }
 
