3122ed59d670ad2509bf23ad00e4e0ad9c8f5a13
Julia Reinhold <julia.reinhold@bitgrip.de>
2022-06-09 13:33:41 +0200

    update list tests with commentw

--- src/Components/List/List.test.tsx
+++ src/Components/List/List.test.tsx
@@ -6,38 +6,51 @@ import { single, singleListWithEmptyId, singleListWithEmptyKey, singleListWithEm
  * @group unit/component
  */
 
-// helper function
-const listItemIsNotInTheDocument = () => {
-  expect(screen.getByRole('list')).toBeInTheDocument();
-  expect(screen.queryByRole('listitem')).not.toBeInTheDocument();
-};
-
 // Let's test the crucial parts and functionalities of a List
+// have a meaningful test description
 describe('should render a list with correct properties:', () => {
+  // render list instance before every test
+  /*
   beforeEach(() => {
     render(<List {...single} />);
   });
+  */
 
+  // test, if we get the expected number of list items
   test('list items', () => {
-    expect(screen.getAllByRole('listitem')).toHaveLength(1);
+    
+    //expect(screen.getAllByRole('listitem')).toHaveLength(1);
   });
 
+  // test, if we get the correct title
   test('title', () => {
-    expect(screen.getByText(single.list[0].title)).toBeInTheDocument();
+    
+    //expect(screen.getByText(single.list[0].title)).toBeInTheDocument();
   });
 
-  // The only test, where we decided to use HTML tag, because we test the link functionality
+  // test, if we use the functionality link
+  // The only test, where we use the HTML tag, because we test the link functionality
   test('link', () => {
-    expect(screen.getByText(single.list[0].title).closest('a')).toHaveAttribute('href', single.list[0].link);
+    
+    //expect(screen.getByText(single.list[0].title).closest('a')).toHaveAttribute('href', single.list[0].link);
   });
 
+  // test, if we use the correct class for the styling of the tag
   test('tag with css class', () => {
-    expect(screen.getByText(single.list[0].tag)).toHaveClass('tags--default');
+    
+    // expect(screen.getByText(single.list[0].tag)).toHaveClass('tags--default');
   });
 });
 
 // Negative tests - it's important also to test, what happens, if there is some data missing!
 describe('should not render list item, if any of properties is missing:', () => {
+  // helper function
+  const listItemIsNotInTheDocument = () => {
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.queryByRole('listitem')).not.toBeInTheDocument();
+  };
+
+  // use meaningful variable names
   test('jobid', () => {
     render(<List {...singleListWithEmptyId} />);
     listItemIsNotInTheDocument();
